91ee7df1b6367bfb026a36d72b098fa2615cda45
Mike West <mike@mikewest.org>
2010-10-17 15:56:41 +0200

    Annotating scripts with Docco.

--- scripts/jslint-to-xml.sh
+++ scripts/jslint-to-xml.sh
@@ -1,59 +1,133 @@
 #!/bin/bash
 
-###########################################################################
 #
-#   CONFIG
-#   ------
+# Using `run-jslint.sh`, we'll generate linting results in an XML format
+# that's similar enough to JUnit to trick Hudson, et al. into accepting
+# them as unit test results that can be rendered after every build.
+#
 
+# Config
+# ------
+
+#
 # The path (relative to this script file) to the JSLint script:
+#
 JSLINT=./run-jslint.sh
 
-# The base of the test name that will be reported in Hudson:
+#
+# The base of the test name that will be reported in Hudson (this will
+# expand to `${TEST_BASENAME}.jslint.[TYPE].[FILENAME]`, for example
+# `org.mikewest.static.jslint.js.javascriptfilename1`):
+#
 TEST_BASENAME="org.mikewest.static"
 
-###########################################################################
+# SCRIPT (no touchies)
+# --------------------
+
 #
-#   SCRIPT (no touchies)
-#   --------------------
+# Store the current script's directory, and use it to generate paths to
+# the JSLinting script (`run-jslint.sh`).
 #
 SCRIPTPATH=`dirname ${0}`
 JSLINT="${SCRIPTPATH}/${JSLINT}"
 
-if [ -a "${1}" ] && [ -d "${2}" ]; then
+#
+# The script expects two arguments: the file to lint, and the path in
+# which report XML files should be created.  If one or the other is
+# missing, print usage info, and exit.
+#
+if [ ! -e "${1}" ] || [ ! -d "${2}" ]; then
+    echo "Usage: ${0} <file to lint> <path to reports>"
+    exit 1
+#
+# Otherwise...
+#
+else
+    #
+    # Generate a temporary report file.  We have to do a bit of bash
+    # hackery to get the data we need: this file is part of that, slightly
+    # dirty, but necessary.  If we can't generate the file, exit.
+    #
     TEMP=`mktemp -t jslintreport.XXXXXX` || {
         echo "FATAL: Couldn't create temp file for reports"
         exit 1
     }
 
+    #
+    # Give the script's two arguments nicer names: `LINTEE` is the file
+    # to be processed, `REPORT_ROOT` is, surprisingly enough, the directory
+    # into which we'll stuff reports.
+    #
     LINTEE=$1
     REPORT_ROOT=$2
 
-    # @TODO:    Extract these into some sort of nice function
+    #
+    # Process `LINTEE` to extract the file extension and basename.  We'll
+    # use both to generate the test name that's displayed in Hudson.
+    #
     LINTEE_TYPE=`echo ${LINTEE} | sed 's#\(.*\)\.\([^\.]*\)$#\2#'`
     LINTEE_BASENAME=`basename ${LINTEE} | sed 's#\(.*\)\.\([^\.]*\)$#\1#'`
 
+    #
+    # Using the extension and basename, generate a test name in the form:
+    # `[BASENAME].jslint.[EXTENSION].[BASENAME]`.  We'll use this to generate
+    # a report XML file of the same name (`[REPORT ROOT]/[TEST NAME].xml`):
+    #
     TEST_BASENAME="${TEST_BASENAME}.jslint.${LINTEE_TYPE}.${LINTEE_BASENAME}"
     REPORT_FILENAME="${REPORT_ROOT}/${TEST_BASENAME}.xml"
 
-    LINT_TIME=`{ time ${JSLINT} "${LINTEE}" 2>&1 1> "${TEMP}"; } 2>&1 | grep real | sed 's#.*m\(.*\)s#\1#'`
+    #
+    # The core; this is stunningly ugly.  So, let's break it down:
+    #
+    # We want the result of `run-jslint.sh [LINTEE]`.  So run that command, and
+    # pipe all it's output (STDOUT/STDERR) into the temporary file we created
+    # earlier (`TEMP`) for processing later on.
+    #
+    # We also, however, want to know how long the test took to run.  That's a
+    # bit of a pain in the ass.  Running the previous command through `time`
+    # will output the time info we want on STDERR.  But not the same STDERR
+    # as the command itself, which means it won't be in the file we've just
+    # filled with data.  The solution is to wrap the entire `time` call in
+    # a subshell, and to process the _list's_ STDOUT.  Grep for "real" to
+    # get the wallclock time, then `sed` out the seconds.
+    #
+    # What a mess.
+    #
+    LINT_TIME=`( time ${JSLINT} "${LINTEE}" 2>&1 1> "${TEMP}"; ) 2>&1 | grep real | sed 's#.*m\(.*\)s#\1#'`
+    #
+    # After all that, read the tempfile back into `LINT_RESULTS`.
+    #
     LINT_RESULTS=$(<"${TEMP}")
 
+    #
+    # If the tempfile contains "No problems found in", then the test passed
+    # without issue.  If not, failure!  Oh, the misery!
+    #
     WAS_FAILURE=`cat "${TEMP}" | grep -v 'No problems found in'`
+
+    #
+    # And now we're completely finished with the temp file.  Kill it.
+    #
     rm ${TEMP}
     
+    #
+    # If the test failed, write out a testsuite XML block into the
+    # `REPORT_FILENAME`, and dump the error messages into a CDATA field.
+    #
     if [ "---${WAS_FAILURE}---" != "------" ]; then
         echo "<testsuite failures='1' time='${LINT_TIME}' errors='1' tests='1' skipped='0' name='${TEST_BASENAME}'>" > $REPORT_FILENAME
         echo "  <testcase time='${LINT_TIME}' name='testJSLint' classname='${TEST_BASENAME}'>" >> $REPORT_FILENAME
         echo "    <error message='${LINTEE_BASENAME} failed JSLint.'><![CDATA[${LINT_RESULTS}]]></error>" >> $REPORT_FILENAME
         echo "  </testcase>" >> $REPORT_FILENAME
         echo "</testsuite>" >> $REPORT_FILENAME
+    #
+    # Otherwise, success!  Dump a testsuite XML block into the file
+    # with 0 failures, and no `error` body.
+    #
     else
         echo "<testsuite failures='0' time='${LINT_TIME}' errors='0' tests='1' skipped='0' name='${TEST_BASENAME}'>" > $REPORT_FILENAME
         echo "  <testcase time='${LINT_TIME}' name='testJSLint' classname='${TEST_BASENAME}'>" >> $REPORT_FILENAME
         echo "  </testcase>" >> $REPORT_FILENAME
         echo "</testsuite>" >> $REPORT_FILENAME
     fi
-else
-    echo "Usage: ${0} <file to lint> <path to reports>"
-    exit 1
 fi
--- scripts/run-jslint.sh
+++ scripts/run-jslint.sh
@@ -1,55 +1,88 @@
 #!/bin/bash
 
-###########################################################################
+# Lints a specified file, using either [Rhino][r] or [Node.js][n] as
+# an engine.  You'll need to ensure that one or the other is
+# installed on any machine on which you'd like to lint.
 #
-#   CONFIG
-#   ------
+# Rhino is included as part of this package (`./lib/js.jar`), so that
+# realy boils down to either installing Node, or Java.  Node will be
+# _much_ faster...
+#
+# [r]: http://www.mozilla.org/rhino/
+# [n]: http://nodejs.org/
+
+# Config
+# ------
 
 # The path (relative to this script file) to the Rhino JAR:
 RHINO=../lib/js.jar
 
-# The path (relative to this script file) to the JSLint JS:
+# The path (relative to this script file) to the JSLint JS files for Rhino:
 RHINO_JSLINT=../lib/rhinoed_jslint.js
+# and Node:
 NODE_JSLINT=../lib/node_jslint.js
 
-###########################################################################
+# Script (No touchies)
+# --------------------
+
 #
-#   SCRIPT (no touchies)
-#   --------------------
+# Store the current script's directory, and use it to generate paths to
+# Rhino and the two JSLinting instances.
 #
 SCRIPTPATH=`dirname ${0}`
 RHINO="${SCRIPTPATH}/${RHINO}"
 RHINO_JSLINT="${SCRIPTPATH}/${RHINO_JSLINT}"
 NODE_JSLINT="${SCRIPTPATH}/${NODE_JSLINT}"
 
-if [ -a "$1" ]; then
+#
+# Write out usage information if we don't have a file to work with.
+#
+if [ ! -e "$1" ]; then
+    echo "Usage: ${0} <file to lint>"
+    exit 1
+#
+# Assuming we _do_ have a file, then store the filename, and parse
+# out the file extension.  We'll use that in a moment to determine
+# how we process the file.
+#
+else
     LINTEE=$1
     LINTEE_TYPE=`echo ${LINTEE} | sed 's#\(.*\)\.\([^\.]*\)$#\2#'`
-    LINTEE_BASENAME=`basename ${LINTEE} | sed 's#\(.*\)\.\([^\.]*\)$#\1#'`
 
+    #
+    # For CSS files, prepend `@charset "UTF-8";`, write to a temp file
+    # and parse _that_ file rather than the original.  This is much simpler
+    # than stripping out the charset from each file when minimizing (It
+    # made sense at the time, anyway...).
+    #
     if [ "${LINTEE_TYPE}" == "css" ]; then
-        #
-        #   For CSS files, prepend `@charset "UTF-8";`, write to a temp file
-        #   and parse _that_ file rather than the original.  This is much simpler
-        #   than stripping out the charset from each file when minimizing.
-        #
         TOPARSE=`mktemp -t csslintreport.XXXXXX` || {
             echo "FATAL: Couldn't create temp file for reports"
             exit 1
         }
         awk -v PREPEND='@charset "UTF-8";' 'BEGIN {print PREPEND}{print}' ${LINTEE} > ${TOPARSE}
         TODISPLAY=$LINTEE
+    #
+    # For everything else (that is, JavaScript, because what else is
+    # there?) run against the file itself.
+    #
     else
         TOPARSE=$LINTEE
         TODISPLAY=$LINTEE
     fi
 
+    #
+    # We've got all the information we need: Now we need to decide
+    # which engine we use.  If `node` exists, use it.  It's amazingly
+    # fast.  Much, much, much faster than Rhino.
+    #
     if [ -n "`which node`" ]; then
         node ${NODE_JSLINT} "${TOPARSE}" "${TODISPLAY}"
+    #
+    # Fallback to Rhino if we have to.  Even though it's ugly and
+    # slow and makes babies cry.
+    #
     else
         java -jar ${RHINO} ${RHINO_JSLINT} "${TOPARSE}" "${TODISPLAY}"
     fi
-else
-    echo "Usage: ${0} <file to lint>"
-    exit 1
 fi
